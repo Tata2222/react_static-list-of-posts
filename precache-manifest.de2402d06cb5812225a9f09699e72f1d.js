0dc18bc6ee5ccff018ab36f6603afff56548e240
Tata2222 <tataolcha@gmail.com>
2020-04-24 20:21:16 +0300

    Updates

---
+++ precache-manifest.de2402d06cb5812225a9f09699e72f1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6b2807878d7d69b52e769048e5bc31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d33730a230b5d29c48d",
+    "url": "./static/css/main.e75d3f48.chunk.css"
+  },
+  {
+    "revision": "6e14b9c9c5b2310e2143",
+    "url": "./static/js/2.52b7182f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.52b7182f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d33730a230b5d29c48d",
+    "url": "./static/js/main.411124ba.chunk.js"
+  },
+  {
+    "revision": "3708a39cbc10f7e3c3bc",
+    "url": "./static/js/runtime-main.4f79b291.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.de2402d06cb5812225a9f09699e72f1d.js"
 );
 
 self.addEventListener('message', (event) => {
